ae428b677a2249b3b4d1b72e95e62ddf2b75c567
emiliia drahomyretska <90069258+emiliiia@users.noreply.github.com>
2023-04-03 04:24:58 +0300

    Task: Display a grid of images with a RecyclerView

--- app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
+++ app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
@@ -21,8 +21,11 @@ package com.example.android.marsrealestate
 import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.example.android.marsrealestate.network.MarsProperty
+import com.example.android.marsrealestate.overview.PhotoGridAdapter
 
 
 //зв'язування даних в Android-додатку
@@ -42,4 +45,12 @@ fun bindImage(imgView: ImageView, imgUrl: String?) {
                 .error(R.drawable.ic_broken_image))
             .into(imgView)
     }
+}
+
+@BindingAdapter("listData")
+fun bindRecyclerView(recyclerView: RecyclerView,
+                     data: List<MarsProperty>?) {
+
+    val adapter = recyclerView.adapter as PhotoGridAdapter
+    adapter.submitList(data)
 }
--- app/src/main/java/com/example/android/marsrealestate/overview/PhotoGridAdapter.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/PhotoGridAdapter.kt
@@ -17,4 +17,57 @@
 
 package com.example.android.marsrealestate.overview
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.marsrealestate.databinding.GridViewItemBinding
+import com.example.android.marsrealestate.network.MarsProperty
 
+
+//розширює ListAdapter для відображення списку MarsProperty у вигляді сітки фотографій
+class PhotoGridAdapter : ListAdapter<MarsProperty,
+    PhotoGridAdapter.MarsPropertyViewHolder>(DiffCallback) {
+
+        //створення нових елементів RecyclerView, тобто віджетів GridViewItemBinding,
+        //коли RecyclerView потребує їх відображення. Функція повертає MarsPropertyViewHolder,
+        //який містить прив’язку до GridViewItemBinding
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoGridAdapter.MarsPropertyViewHolder {
+            return MarsPropertyViewHolder(GridViewItemBinding.inflate(
+                LayoutInflater.from(parent.context)))
+        }
+
+         //отримує позицію елемента і встановлює значення відповідного об’єкта MarsProperty
+        override fun onBindViewHolder(holder: PhotoGridAdapter.MarsPropertyViewHolder, position: Int) {
+            val marsProperty = getItem(position)
+            holder.bind(marsProperty)
+    }
+
+    companion object DiffCallback : DiffUtil.ItemCallback<MarsProperty>() {
+        override fun areItemsTheSame(oldItem: MarsProperty,
+                                     newItem: MarsProperty): Boolean {
+            return oldItem === newItem
+        }
+
+        override fun areContentsTheSame(oldItem: MarsProperty,
+                                        newItem: MarsProperty): Boolean {
+            return oldItem.id == newItem.id
+        }
+    }
+
+
+    //містить прив’язку до GridViewItemBinding і функцію bind,
+    // яка прив’язує MarsProperty до GridViewItemBinding та виконує відображення
+    class MarsPropertyViewHolder(private var binding:
+                                 GridViewItemBinding
+    ):
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(marsProperty: MarsProperty) {
+            binding.property = marsProperty
+            binding.executePendingBindings()
+        }
+    }
+
+}
