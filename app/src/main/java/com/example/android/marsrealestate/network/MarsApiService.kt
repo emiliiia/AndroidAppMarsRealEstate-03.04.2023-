1393b0e9d50abb23fa0ee0013d9ed426afec4ed2
emiliia drahomyretska <90069258+emiliiia@users.noreply.github.com>
2023-04-03 03:48:41 +0300

    Task: Parse the JSON response with Moshi

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -17,18 +17,23 @@
 
 package com.example.android.marsrealestate.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Call
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 
 // Оголошуємо константу з базовою URL-адресою для запитів
 private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com/"
 
-// Створюємо об'єкт Retrofit з конвертером для рядків та базовою URL-адресою
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
@@ -36,7 +41,7 @@ private val retrofit = Retrofit.Builder()
 interface MarsApiService {
     @GET("realestate")
     fun getProperties():
-            Call<String>
+            Call<List<MarsProperty>>
 }
 
 // Створюємо об'єкт-синглтон для отримання сервісу через lazy-ініціалізацію
